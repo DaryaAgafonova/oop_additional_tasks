fc2c2bce827ace0d977c09d7e50c4eeb926f23d1
daryaagafonova <ADashyta2013@yandex.ru>
2023-08-13 17:31:35 +0300

    A class was written with writing text to a file log.txt, and a file was created.

--- lesson3/logger.py
+++ lesson3/logger.py
@@ -8,8 +8,19 @@
 
 
 class Logger:
-    pass
+
+    def __init__(self, filename=""):
+        """ конструктор, принимающий имя файла, в который будет производиться запись логов """
+        self.filename = filename
+
+    def __call__(self, message):
+        """ магический метод, который позволяет использовать объект класса Logger как функцию,
+принимающую сообщение и записывающую его в файл """
+        self.message = message
+        sms = open(self.filename, "w")
+        sms.write(self.message)
+        sms.close()
 
 
 logger = Logger("log.txt")
-logger("This is a test message.")
+logger("This is a test message.") # в папке lesson3 создался файл log.txt с текстом This is a test message.
